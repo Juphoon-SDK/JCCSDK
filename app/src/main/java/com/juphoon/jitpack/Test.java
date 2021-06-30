64ad4afb69d35cf550423a36514c8e4e22a77c9a
libo <juphoonnative@163.com>
2021-06-30 20:01:47 +0800

    version: 2.8.12, 202106301

---
+++ app/src/main/java/com/juphoon/jitpack/Test.java
@@ -0,0 +1,14 @@
+package com.juphoon.jitpack;
+
+import com.juphoon.cloud.JCClient;
+
+/**
+ * Date: 2021/6/30
+ * Time: 7:33 PM
+ * Author: afei
+ */
+public class Test {
+    void test() {
+        JCClient jcClient;
+    }
+}
